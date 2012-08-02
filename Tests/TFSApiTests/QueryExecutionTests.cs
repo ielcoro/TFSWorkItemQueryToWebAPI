b2257ba95c9a349cd8b3964a6a9f9405cd6ba787
iCe <iceclow@gmail.com>
2012-08-02 21:22:54 +0200

    Added TFS Exploratory Tests
    Tests to know about TFS API Interfaces

---
+++ Tests/TFSApiTests/QueryExecutionTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TFSApiTests
+{
+    [TestClass]
+    public class QueryExecutionTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
