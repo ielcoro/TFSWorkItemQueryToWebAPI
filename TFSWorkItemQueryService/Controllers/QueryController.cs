e95e5fde5713def15115677c0209e96e4de76f82
iCe <iceclow@gmail.com>
2012-08-02 14:38:24 +0200

    Initial Query Controller

---
+++ TFSWorkItemQueryService/Controllers/QueryController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TFSWorkItemQueryService.Controllers
+{
+    public class QueryController : ApiController
+    {
+        // GET api/query
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/query/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/query
+        public void Post(string value)
+        {
+        }
+
+        // PUT api/query/5
+        public void Put(int id, string value)
+        {
+        }
+
+        // DELETE api/query/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TFSWorkItemQueryService/Repository/TFSRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+
+namespace TFSWorkItemQueryService.Repository
+{
+    public class TFSRepository
+    {
+        private TfsTeamProjectCollection Connect(Uri tfsAddress)
+        {
+            var tfsServer = new TfsTeamProjectCollection(tfsAddress);
+
+            tfsServer.ClientCredentials = new TfsClientCredentials(new WindowsCredential());
+            tfsServer.Connect(Microsoft.TeamFoundation.Framework.Common.ConnectOptions.IncludeServices);
+
+            return tfsServer;
+        }
+
+        private IEnumerable<WorkItemLink> ExecuteQuery(Uri tfsAddress, string teamProject, string workItemQuery)
+        {
+            var tfsServer = Connect(tfsAddress);
+
+            var workItemStore = tfsServer.GetService<WorkItemStore>();
+
+
+        }
+    }
+}
