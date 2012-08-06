b8e86ec52fe56e2c7e2f39b9511dbd2b08525497
iCe <iceclow@gmail.com>
2012-08-06 18:36:23 +0200

    Added QueryRunnerTests and QueryRunner

---
+++ TFSWorkItemQueryService/Repository/QueryRunner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+
+namespace TFSWorkItemQueryService.Repository
+{
+    public class QueryRunner : IQueryRunner
+    {
+
+        public IEnumerable<WorkItem> RunQuery(QueryDefinition queryDefinition)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tests/UnitTests/QueryRunnerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class QueryRunnerTests
+    {
+        [TestMethod]
+        public void QueryRunFromQueryDefinitonShouldReturnWorkItems()
+        {
+            
+        }
+    }
+}
