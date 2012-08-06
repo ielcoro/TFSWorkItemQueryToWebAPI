f2e20f7cb906d4a478d25ca7890a2cec966cc5f3
iCe <iceclow@gmail.com>
2012-08-06 18:43:17 +0200

    QueryRunner Tests in red
    QueryRunFromQueryDefinitonShouldReturnWorkItems is in red

--- Tests/UnitTests/QueryRunnerTests.cs
+++ Tests/UnitTests/QueryRunnerTests.cs
@@ -1,15 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client.Fakes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TFSWorkItemQueryService.Repository;
+using System.Linq;
+using Microsoft.QualityTools.Testing.Fakes;
 
 namespace UnitTests
 {
     [TestClass]
     public class QueryRunnerTests
     {
+        ShimQueryDefinition queryDefinition;
+        IDisposable shimContext;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            shimContext = ShimsContext.Create();
+
+            queryDefinition = new ShimQueryDefinition();
+            queryDefinition.QueryTextGet = () => "SELECT System.ID, System.Title from workitems";
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            shimContext.Dispose();
+        }
+
         [TestMethod]
         public void QueryRunFromQueryDefinitonShouldReturnWorkItems()
         {
-            
+            var queryRunner = new QueryRunner();
+
+            IEnumerable<WorkItem> workItems = queryRunner.RunQuery(queryDefinition);
+
+            Assert.IsTrue(workItems.Any());
         }
     }
 }
