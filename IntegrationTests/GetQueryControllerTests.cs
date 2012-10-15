e5a27c568480819e4dc2e8bbed07f4727b539032
iCe <iceclow@gmail.com>
2012-10-15 18:11:38 +0200

    Init integration testing for controllers

---
+++ IntegrationTests/GetQueryControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntegrationTests
+{
+    [TestClass]
+    public class RunQueryControllerTests
+    {
+        [TestMethod]
+        public void RunQueryControllerTestReturnsWorkItemsInXML()
+        {
+            Assert.Fail("Not implemented");
+        }
+    }
+}
