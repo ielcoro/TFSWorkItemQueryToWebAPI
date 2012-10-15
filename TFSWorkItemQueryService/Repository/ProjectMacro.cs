e5c1e3e390b5fd65f02077b630bb027be4353fbc
iCe <iceclow@gmail.com>
2012-10-15 12:36:56 +0200

    Macro Definitions Tests

---
+++ TFSWorkItemQueryService/Repository/ProjectMacro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TFSWorkItemQueryService.Repository
+{
+    public class ProjectMacro
+        : IMacro
+    {
+        public string GetValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ Tests/UnitTests/MacroDefinitionsTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TFSWorkItemQueryService.Repository;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class MacroDefinitionsTests
+    {
+        [TestMethod]
+        public void ProjectMacroDefinitionShouldSetMacroName()
+        {
+            var projectMacro = new ProjectMacro();
+
+            Assert.AreEqual("Project", projectMacro.Name);
+        }
+
+        [TestMethod]
+        public void ProjectMacroDefinitionShouldGetValueFromContext()
+        {
+            var projectMacro = new ProjectMacro();
+
+            
+        }
+
+    }
+}
