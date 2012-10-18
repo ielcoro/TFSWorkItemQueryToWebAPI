f1963ba0e22051b8aee0cbbbc141e24bcf2f158b
iCe <iceclow@gmail.com>
2012-10-18 18:47:11 +0200

    Created dynamic getitem for template fields

---
+++ TFSWorkItemQueryService/Models/WorkItemModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace TFSWorkItemQueryService.Repository.Model
+{
+    public class WorkItemModel
+        : DynamicObject
+    {
+        private IEnumerable<Field> workItemFields;
+
+        public WorkItemModel(WorkItem workItem)
+        {
+            this.workItemFields = workItem.Fields.OfType<Field>();
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            bool exists = false;
+
+            if (exists = workItemFields.Any(x => x.Name == binder.Name))
+            {
+                result = (from f in workItemFields
+                          where f.Name == binder.Name
+                          select f.Name).SingleOrDefault();
+            }
+            else
+                exists = base.TryGetMember(binder, out result);
+
+            return exists;
+        }
+
+    }
+}
--- Tests/UnitTests/WorkItemSerializationTests.cs
+++ Tests/UnitTests/WorkItemSerializationTests.cs
@@ -1,14 +1,66 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.TeamFoundation.WorkItemTracking.Client.Fakes;
+using TFSWorkItemQueryService.Repository;
+using TFSWorkItemQueryService.Repository.Model;
 
 namespace UnitTests
 {
     [TestClass]
     public class WorkItemSerializationTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        IDisposable shimContext;
+
+        [TestInitialize]
+        public void Initialize()
         {
+            shimContext = ShimsContext.Create();
         }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            shimContext.Dispose();
+        }
+
+        [TestMethod]
+        public void WorkItemNativePropertiesShouldBeAccessedAsSuch()
+        {
+            var workItem = new ShimWorkItem();
+
+            workItem.TitleGet = () => "Title";
+            workItem.DescriptionGet = () => "Description";
+
+            dynamic workItemWrapper = new WorkItemModel(workItem);
+
+            Assert.AreEqual("Title", workItemWrapper.Title);
+            Assert.AreEqual("Description", workItemWrapper.Description);
+        }
+
+        [TestMethod]
+        public void WorkItemTemplatePropertiesShouldBeAccessedAsNativeProperties()
+        {
+            var workItem = new ShimWorkItem();
+
+            var fieldCollectionFake = new ShimFieldCollection();
+
+            fieldCollectionFake.Bind(new Field[] 
+            {
+                new ShimField() 
+                {
+                    NameGet = () => "CustomField", 
+                    ValueGet = () => "CustomValue"
+                }
+            });
+
+            workItem.FieldsGet = () => fieldCollectionFake;
+
+            dynamic workItemWrapper = new WorkItemModel(workItem);
+
+            Assert.AreEqual("CustomValue", workItemWrapper.CustomField);
+        }
+
     }
 }
